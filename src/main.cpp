a34968f73fb8d596394cbd30c4d79d2c63618d45
changsun20 <110759360+changsun20@users.noreply.github.com>
2026-04-01 16:30:12 -0400

    feat: finish -l -c feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,35 @@
-#include "CLI/CLI.hpp"
 #include <CLI/CLI.hpp>
-#include <cstdio>
+#include <cstdint>
 #include <fstream>
+#include <ios>
 #include <iostream>
 #include <ostream>
-#include <sstream>
-#include <string_view>
+#include <string>
+
+namespace wc {
+uint64_t count_bytes(std::ifstream &input) {
+    char buffer[1024];
+
+    uint64_t byte_count = 0;
+    while (input.read(buffer, sizeof(buffer))) {
+        byte_count += sizeof(buffer);
+    }
+    byte_count += input.gcount();
+
+    return byte_count;
+}
+
+uint64_t count_lines(std::ifstream &input) {
+    uint64_t line_count = 0;
+
+    std::string line;
+    while (std::getline(input, line)) {
+        line_count += 1;
+    }
+
+    return line_count;
+}
+} // namespace wc
 
 int main(int argc, char **argv) {
     CLI::App app;
@@ -21,31 +45,15 @@ int main(int argc, char **argv) {
 
     CLI11_PARSE(app, argc, argv);
 
-    if (cflag) {
-        char buffer[1024];
-        std::ifstream file(filename, std::ios::binary);
-
-        int cnt = 0;
-        while (file.read(buffer, 1024)) {
-            cnt += file.gcount();
-        }
-        cnt += file.gcount();
-
-        std::cout << cnt << std::endl;
-
-        std::cout << filename << std::endl;
-    }
-
     if (lflag) {
         std::ifstream file(filename);
-
-        int line_cnt = 0;
-
-        char buffer[1024];
-        while (file.getline(buffer, 1024)) {
-            line_cnt += 1;
-        }
-
-        std::cout << line_cnt << std::endl;
+        std::cout << " " << wc::count_lines(file);
     }
+
+    if (cflag) {
+        std::ifstream file(filename, std::ios::binary);
+        std::cout << " " << wc::count_bytes(file);
+    }
+
+    std::cout << " " << filename << std::endl;
 }
